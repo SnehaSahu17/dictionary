f70b55c72345cccd4dc320f1c0f4340850f7197f
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2022-12-14 01:38:13 +0530

    third commit

--- script.js
+++ script.js
@@ -29,9 +29,29 @@ btn.addEventListener("click", () => {
             sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
         .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+            result.innerHTML = `<p class="error">Couldn't Find The Word</p>`;
         });
 });
 function playSound() {
     sound.play();
 }
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
